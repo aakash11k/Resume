e8316008e26c0d3289f6cd69c0dbd7d6ad12eba0
KARTHIKEYAN AAKASH <130399526+aakash11k@users.noreply.github.com>
2023-06-10 11:05:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const contactSection = document.querySelector("#contact");
+const socialLinks = document.querySelectorAll(".social");
+
+socialLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.target;
+    contactSection.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+const footer = document.querySelector("footer");
+const currentYear = new Date().getFullYear();
+footer.innerHTML = `<hr /><center><p>&copy; ${currentYear} Aakash Karthikeyan</p></center><hr />`;
+
+const animateSections = () => {
+  const aboutMeSection = document.querySelector("#about-me");
+  const personalProjectsSection = document.querySelector("#personal-projects");
+  const softSkillsSection = document.querySelector("#soft-skills");
+  const hobbiesSection = document.querySelector("#hobbies");
+  aboutMeSection.classList.add("float-animation");
+  personalProjectsSection.classList.add("float-animation");
+  softSkillsSection.classList.add("float-animation");
+  hobbiesSection.classList.add("slide-in");
+};
+
+animateSections();
